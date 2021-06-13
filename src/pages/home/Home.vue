4baa72ccb4853575bb3402651365f7dc0c90ba2b
stevehum <stevehum@192.168.0.7>
2021-06-13 20:46:54 +0800

    add

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="home">Home</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+};
+</script>
+<style>
+.home {
+  font-size: 50px;
+}
+</style> 
