e71b32033dc6ca4a2a225db402340b91629927bb
stevehum <stevehum@192.168.0.7>
2021-06-15 00:49:40 +0800

    iconList

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,16 +2,19 @@
   <div>
     <home-header></home-header>
     <Home-swiper></home-swiper>
+    <Home-icons></home-icons>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
-    HomeSwiper: HomeSwiper
+    HomeSwiper: HomeSwiper,
+    HomeIcons: HomeIcons
   }
 }
 </script>
