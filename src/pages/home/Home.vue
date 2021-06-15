a92cae67a877fff11886badafd00a50b43dfd4db
stevehum <stevehum@MacBook-Pro.local>
2021-06-15 20:34:42 +0800

    add weekend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
     <Home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></Home-recommend>
+    <home-weekend></Home-weekend>
   </div>
 </template>
 <script>
@@ -11,13 +12,15 @@ import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
     HomeSwiper: HomeSwiper,
     HomeIcons: HomeIcons,
-    HomeRecommend: HomeRecommend
+    HomeRecommend: HomeRecommend,
+    HomeWeekend: HomeWeekend
   }
 }
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom"
+        v-for="item of list"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      list: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/79faa5a73731e84a7731db49d5baa91e.jpeg',
+        title: '古龙峡漂流',
+        desc: '广东漂流看清远，清远漂流看古龙'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/79faa5a73731e84a7731db49d5baa91e.jpeg',
+        title: '古龙峡漂流',
+        desc: '广东漂流看清远，清远漂流看古龙'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/79faa5a73731e84a7731db49d5baa91e.jpeg',
+        title: '古龙峡漂流',
+        desc: '广东漂流看清远，清远漂流看古龙'
+      }, {
+        id: '0004',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/79faa5a73731e84a7731db49d5baa91e.jpeg',
+        title: '古龙峡漂流',
+        desc: '广东漂流看清远，清远漂流看古龙'
+      }, {
+        id: '0005',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/79faa5a73731e84a7731db49d5baa91e.jpeg',
+        title: '古龙峡漂流',
+        desc: '广东漂流看清远，清远漂流看古龙'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  line-height: .8rem
+  background: #eee
+  text-indent: .2rem
+.item-img-wrapper
+  overflow: hidden
+  height: 0
+  padding-bottom:31%
+  .item-img
+    width: 100%
+.item-info
+  padding: .1rem
+  .item-title
+    line-height: .54rem
+    font-size: .32rem
+    ellipsis()
+  .item-desc
+    line-height: .4rem
+    color: #ccc
+    ellipsis()
+</style>
