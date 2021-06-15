88acf9b8cb6d64626fce79397414dbba7653d587
stevehum <stevehum@MacBook-Pro.local>
2021-06-15 19:59:15 +0800

    add recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,19 +2,22 @@
   <div>
     <home-header></home-header>
     <Home-swiper></home-swiper>
-    <Home-icons></home-icons>
+    <home-icons></home-icons>
+    <home-recommend></Home-recommend>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
 export default {
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
     HomeSwiper: HomeSwiper,
-    HomeIcons: HomeIcons
+    HomeIcons: HomeIcons,
+    HomeRecommend: HomeRecommend
   }
 }
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,95 @@
+<template>
+<div>
+  <div class="title">热销推荐</div>
+  <ul>
+    <router-link
+      tag="li" class="item border-bottom"
+      v-for="item of recommendList" :key="item.id"
+      :to="'/detail/' + item.id"
+    >
+      <img class="item-img" :src="item.imgUrl" />
+      <div class="item-info">
+        <p class="item-title">{{item.title}}</p>
+        <p class="item-desc">{{item.desc}}</p>
+        <button class="item-button">查看详情</button>
+      </div>
+    </router-link>
+  </ul>
+
+</div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2106/b3/b378c7fe908c5aada3.img.jpg_200x200_b4980c01.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2106/b3/b378c7fe908c5aada3.img.jpg_200x200_b4980c01.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2106/b3/b378c7fe908c5aada3.img.jpg_200x200_b4980c01.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0004',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2106/b3/b378c7fe908c5aada3.img.jpg_200x200_b4980c01.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0005',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2106/b3/b378c7fe908c5aada3.img.jpg_200x200_b4980c01.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0006',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2106/b3/b378c7fe908c5aada3.img.jpg_200x200_b4980c01.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  margin-top:.2rem
+  line-height:.8rem
+  background:#eee
+  text-indent:.2rem
+.item
+  overflow: hidden
+  display: flex
+  height: 1.9rem
+  .item-img
+    width: 1.7rem
+    height: 1.7rem
+    padding: .1rem
+  .item-info
+    flex: 1
+    padding: .1rem
+    min-width:0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      line-height:.44rem
+      margin-top: .16rem
+      background: #ff9300
+      padding: 0 .2rem
+      border-radius: .06rem
+      color: #fff
+</style>
